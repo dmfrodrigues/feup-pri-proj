130b87227a0b422b952cdbfa4b43eb20ebad0cc7
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-10-29 21:05:47 +0100

    Finished getting documents

--- data/eurlex-document.py
+++ data/eurlex-document.py
@@ -1,7 +1,10 @@
+import time, sys
+
 import queue
-import time
 import urllib.request
 from threading import Thread
+
+import json
 
 def perform_web_requests(addresses, no_workers):
     class Worker(Thread):
@@ -13,9 +16,9 @@ def perform_web_requests(addresses, no_workers):
         def run(self):
             while True:
                 content = self.queue.get()
-                print("Processing " + content)
                 if content == "":
                     break
+                print("Processing " + content, file=sys.stderr)
                 request = urllib.request.Request(content)
                 response = urllib.request.urlopen(request)
                 self.results.append(response.read())
@@ -47,6 +50,15 @@ def perform_web_requests(addresses, no_workers):
 
 urls = [ "http://api.epdb.eu/eurlex/document/?offset=" + str(i) for i in range(0, 139000, 1000) ]
 
+start = time.time()
 results = perform_web_requests(urls, 8)
+end = time.time()
+print("Processed links in " + str(end-start) + " seconds", file=sys.stderr)
 
-print(results.keys())
+data = dict()
+for s in results:
+    d = json.loads(s)
+    if "next"     in d: del d["next"]
+    if "previous" in d: del d["previous"]
+    data.update(d)
+print(json.dumps(data, sort_keys=True, indent=2))
