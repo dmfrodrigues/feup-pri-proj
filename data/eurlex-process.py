c519130d685f2652c4a0810a3cebc5817e7a0dbc
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-11-06 02:29:27 +0000

    Compressed relationships

--- data/eurlex-process.py
+++ data/eurlex-process.py
@@ -48,6 +48,21 @@ for key in input_data:
     url = entry["eurlex_perma_url"]
     celex = get_celex_from_url(url)
 
+    rels = []
+    for rel in entry["relationships"]:
+        rel_link = rel["link"]
+        if rel_link == '': continue
+
+        if rel_link.startswith("http://eur-lex.europa.eu/http://eur-lex.europa.eu/LexUriServ/LexUriServ.do?uri=CONSLEG"): continue
+        if rel_link == "http://eur-lex.europa.eu/Result.do?RechType=RECH_celex&amp;lang=en&amp;code=[2~192E066": continue
+
+        rel_celex = get_celex_from_url_rel(rel_link)
+        if rel_celex == '': continue
+        if rel_celex == None:
+            print(f"CELEX {celex}, FAILED TO MATCH REL {rel_link}", file=sys.stderr)
+        else:
+            rels.append(rel_celex)
+
     newEntry = {
         "celex": celex,
         
@@ -61,12 +76,12 @@ for key in input_data:
         "end_validity": entry["end_validity"],
         
         "addressee": entry["addressee"],
+
         "subject_matter": [v["subject_matter"] for v in entry["subject_matter"]],
-    
         "directory_codes": [v["directory_code"] for v in entry["directory_codes"]],
         "eurovoc_descriptors": [v["eurovoc_descriptor"] for v in entry["eurovoc_descriptors"]],
         "legal_basis": [v["legal_basis"] for v in entry["legal_basis"]],
-        "relationships": entry["relationships"],
+        "relationships": rels
     }
 
     data[celex] = newEntry
@@ -98,15 +113,40 @@ for celex in data:
     
     data[celex]["legal_basis"] = legal_basis
 
+    # RELATIONSHIPS
+    relationships_old = data[celex]["relationships"]
+    relationships = []
+
+    for s in relationships_old:
+        if s == "" or s == "-" or s == "[2~192E066": continue
+
+        m = re.match(r'([a-zA-Z0-9\(\)\-\/]+)( )*[\-]+.*', s)
+        if m != None: relationships.append(m.group(1)); continue
+        
+        if s in LEGAL_BASIS_REPLACEMENTS: relationships.append(LEGAL_BASIS_REPLACEMENTS[s]); continue
+
+        relationships.append(s)
+    
+    for rel in relationships:
+        if rel not in data:
+            print("{}: relationship {} not available".format(celex, rel), file=sys.stderr)
+            # if re.match(r'([a-zA-Z0-9\(\)\-\/]+)', rel) == None:
+            #     print("{}: relationship {} not available and is strange".format(celex, rel), file=sys.stderr)
+            pass
+    
+    data[celex]["relationships"] = relationships
+
     # OTHERS
     assert "".join(data[celex]["subject_matter"     ]).find(";") == -1
     assert "".join(data[celex]["directory_codes"    ]).find(";") == -1
     assert "".join(data[celex]["eurovoc_descriptors"]).find(";") == -1
     assert "".join(data[celex]["legal_basis"        ]).find(";") == -1
+    assert "".join(data[celex]["relationships"      ]).find(";") == -1
 
     data[celex]["subject_matter"     ] = ";".join(data[celex]["subject_matter"     ])
     data[celex]["directory_codes"    ] = ";".join(data[celex]["directory_codes"    ])
     data[celex]["eurovoc_descriptors"] = ";".join(data[celex]["eurovoc_descriptors"])
     data[celex]["legal_basis"        ] = ";".join(data[celex]["legal_basis"        ])
+    data[celex]["relationships"      ] = ";".join(data[celex]["relationships"      ])
 
     w.writerow([data[celex][col] for col in columns])
