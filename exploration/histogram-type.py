729ad98f8cf6853c85346045a52db64544cba67d
Joao Sousa <joaosousa.pt@gmail.com>
2021-11-11 18:26:19 +0000

    Deleting histogram-subject.py, histogram-type.py files

--- exploration/histogram-type.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv("../data/eurlex/filtered.csv", sep=",", quotechar="'")
-
-print(df.groupby('form').size().plot(kind='bar', rot=0))
-
-plt.show(block=True)
