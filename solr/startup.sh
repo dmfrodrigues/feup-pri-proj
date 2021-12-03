d2916e0e13338e99ad94e86dea006206919c2f46
Rafael Ribeiro <45789887+up201806330@users.noreply.github.com>
2021-12-03 16:20:50 +0000

    New data

--- solr/startup.sh
+++ solr/startup.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# EXAMPLE SCRIPT
+######
+# precreate-core courses
+
+# # Start Solr in background mode so we can use the API to upload the schema
+# solr start
+
+# sleep 2
+
+# # Schema definition via API
+# curl -X POST -H 'Content-type:application/json' \
+#     --data-binary @/data/simple_schema.json \
+#     http://localhost:8983/solr/courses/schema
+
+# # Populate collection
+# bin/post -c courses /data/meic_courses.json
+
+# # Restart in foreground mode so we can access the interface
+# solr restart -f
+######
+
 precreate-core courses
 
 # Start Solr in background mode so we can use the API to upload the schema
@@ -8,12 +29,12 @@ solr start
 sleep 2
 
 # Schema definition via API
-curl -X POST -H 'Content-type:application/json' \
-    --data-binary @/data/simple_schema.json \
-    http://localhost:8983/solr/courses/schema
+# curl -X POST -H 'Content-type:application/json' \
+#     --data-binary @/data/simple_schema.json \
+#     http://localhost:8983/solr/courses/schema
 
 # Populate collection
-bin/post -c courses /data/meic_courses.json
+bin/post -c courses /data/combined.csv
 
 # Restart in foreground mode so we can access the interface
 solr restart -f
