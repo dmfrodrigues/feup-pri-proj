24056ff2c18b521d027dfdc469112148d3fa99d9
Rafael Ribeiro <45789887+up201806330@users.noreply.github.com>
2021-12-04 12:44:42 +0000

    Fixed setup
    - TO RUN: Add `combined.csv` to solr/data directory and run `docker-compose up --build`on the root of the project

--- solr/startup.sh
+++ solr/startup.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 
-# EXAMPLE SCRIPT
-######
-# precreate-core courses
-
-# # Start Solr in background mode so we can use the API to upload the schema
-# solr start
-
-# sleep 2
-
-# # Schema definition via API
-# curl -X POST -H 'Content-type:application/json' \
-#     --data-binary @/data/simple_schema.json \
-#     http://localhost:8983/solr/courses/schema
-
-# # Populate collection
-# bin/post -c courses /data/meic_courses.json
-
-# # Restart in foreground mode so we can access the interface
-# solr restart -f
-######
-
-precreate-core courses
+precreate-core docs
 
 # Start Solr in background mode so we can use the API to upload the schema
 solr start
@@ -30,11 +9,15 @@ sleep 2
 
 # Schema definition via API
 # curl -X POST -H 'Content-type:application/json' \
-#     --data-binary @/data/simple_schema.json \
+#     --data-binary @/data/schema.json \
 #     http://localhost:8983/solr/courses/schema
 
 # Populate collection
-bin/post -c courses /data/combined.csv
+curl 'http://localhost:8983/solr/docs/update/csv?commit=true&encapsulator="' \
+    --data-binary @/data/combined.csv \
+    -H 'Content-type:application/csv'
+
+sleep 2
 
 # Restart in foreground mode so we can access the interface
 solr restart -f
