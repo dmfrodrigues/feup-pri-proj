d2c9327a089b74970ae94a2f904a61a9914f1543
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2022-01-16 17:28:30 +0000

    Filter out relations that do not exist

--- data/eurlex-combine.py
+++ data/eurlex-combine.py
@@ -9,6 +9,11 @@ def urlencode(s):
 def pathencode(s):
     return urlencode(s).replace("%28", "(").replace("%29", ")")
 
+def filterRelationships(col, keys):
+    l = col.split(";")
+    l = [s for s in l if s in keys]
+    return ";".join(l)
+
 assert len(sys.argv) == 3
 
 filtered_file = open(sys.argv[1], "r")
@@ -21,6 +26,8 @@ w = csv.writer(sys.stdout)
 r = csv.reader(filtered_file)
 columns = next(r)
 celex_idx = columns.index("celex")
+relationships_idx = columns.index("relationships")
+legalbasis_idx = columns.index("legal_basis")
 columns.append("text")
 w.writerow(columns)
 
@@ -36,6 +43,10 @@ for row in r:
         while "\n\n\n" in s: s = s.replace("\n\n\n", "\n\n")
         s = s.replace(u'\u00A0', ' ')
         row.append(s)
+
+        row[relationships_idx] = filterRelationships(row[relationships_idx], keys)
+        row[legalbasis_idx] = filterRelationships(row[legalbasis_idx], keys)
+
         w.writerow(row)
     i += 1
 
