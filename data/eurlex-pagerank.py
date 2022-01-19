0830afc9530412158a6422118d86a42df2caa4b0
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2022-01-19 01:44:01 +0000

    Stuff

--- data/eurlex-pagerank.py
+++ data/eurlex-pagerank.py
@@ -3,7 +3,15 @@ import networkx as nx
 from networkx.algorithms.link_analysis.pagerank_alg import pagerank
 from time import time
 
-r = csv.reader(sys.stdin)
+textsFilePath = sys.argv[1]
+docsFilePath  = sys.argv[2]
+
+keys = set()
+with open(textsFilePath) as f:
+    for line in f:
+        keys.add(line.strip())
+
+r = csv.reader(open(docsFilePath, 'r'))
 w = csv.writer(sys.stdout)
 
 columns = next(r)
@@ -15,18 +23,14 @@ adj = dict()
 rows = []
 
 for row in r:
-    rows.append(row)
     celex = row[celex_idx]
+    if celex not in keys: continue
+
+    rows.append([celex])
     legal_basis = set(row[legal_basis_idx].split(";"))
     relationships = set(row[relationships_idx].split(";"))
     adjacent = set.union(legal_basis, relationships)
-    adj[celex] = adjacent
-
-print("Making edges consistent", file=sys.stderr)
-
-keys = set(adj.keys())
-for k in keys:
-    adj[k] = adj[k] & keys
+    adj[celex] = adjacent & keys
 
 print("Generating graph", file=sys.stderr)
 
@@ -34,6 +38,8 @@ G = nx.Graph()
 G.add_nodes_from(keys)
 for u in keys:
     for v in adj[u]:
+        assert u in G
+        assert v in G
         G.add_edge(u, v)
 
 print(f"Graph has {G.number_of_nodes()} nodes and {G.number_of_edges()} edges", file=sys.stderr)
@@ -41,15 +47,14 @@ print(f"Graph has {G.number_of_nodes()} nodes and {G.number_of_edges()} edges", 
 print("Starting PageRank", file=sys.stderr)
 
 start = time()
-rank = pagerank(G, alpha=0.85, max_iter=1000, tol=1e-19)
+rankVector = pagerank(G, alpha=0.85, max_iter=500, tol=1e-19)
 finish = time()
 
-print(f"Done PageRank, took {finish-start:.3f}s; got {len(rank)} ranks out of {len(rows)} documents", file=sys.stderr)
+print(f"Done PageRank, took {finish-start:.3f}s; got {len(rankVector)} ranks out of {len(rows)} documents", file=sys.stderr)
 
-columns.append("rank")
-w.writerow(columns)
+w.writerow(["celex", "rank"])
 
 for row in rows:
     celex = row[celex_idx]
-    row.append(f"{rank[celex]:.15f}")
-    w.writerow(row)
+    rank = rankVector[celex]
+    w.writerow([celex, f"{rank:.15f}"])
